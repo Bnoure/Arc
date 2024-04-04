7cdde5352ecf97ae85ab6e291bd3f7a300a1e7b8
Bnoure <noureddine.benkerroum@gmail.com>
2024-04-04 10:36:03 +0200

    Améliorer la recherche de ville pour être insensible à la casse

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -3,7 +3,9 @@ class HotelsController < ApplicationController
     if params[:search] && params[:search][:city].present?
       @search_params = search_params.to_h
       @search_params[:start_date], @search_params[:end_date] = @search_params[:start_date].split(' au ')
-      @hotels = Hotel.joins(:rooms).where(city: @search_params[:city]).where('rooms.capacity >= ?', @search_params[:capacity]).distinct
+      @hotels = Hotel.joins(:rooms).where('city ILIKE ?', "%#{@search_params[:city]}%").where('rooms.capacity >= ?', @search_params[:capacity]).distinct
+      @city = Hotel.find_by('city ILIKE ?', "%#{@search_params[:city]}%").city
+      @search_params[:city] = @city
     else
       @hotels = Hotel.all
     end
