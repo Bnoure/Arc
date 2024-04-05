d01714d41c9f0e94f5d9250a4c14343eb2062f84
noureddine benkerroum <noureddine.benkerroum@gmail.com>
2024-04-05 20:34:47 +0200

    Mise à jour des styles de la page _home.scss et _resa.scss, et des vues index.html.erb, show.html.erb, et _footer.html.erb

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -4,7 +4,16 @@ class HotelsController < ApplicationController
       @search_params = search_params.to_h
       @search_params[:start_date], @search_params[:end_date] = @search_params[:start_date].split(' au ')
       @hotels = Hotel.joins(:rooms).where('city ILIKE ?', "%#{@search_params[:city]}%").where('rooms.capacity >= ?', @search_params[:capacity]).distinct
-      @city = Hotel.find_by('city ILIKE ?', "%#{@search_params[:city]}%").city
+      hotel = Hotel.find_by('city ILIKE ?', "%#{@search_params[:city]}%")
+      if hotel
+        @city = hotel.city
+      else
+        respond_to do |format|
+          format.html { redirect_to request.referrer || root_path, alert: "La ville n'est pas trouvée." }
+          format.js { render js: "Swal.fire('Erreur', 'La ville n\'est pas trouvée.', 'error')" }
+        end
+        return
+      end
       @search_params[:city] = @city
     else
       @hotels = Hotel.all
@@ -23,7 +32,7 @@ class HotelsController < ApplicationController
 
   private
   def search_params
-    params.require(:search).permit(:city, :capacity, :start_date)
+    params.require(:search).permit(:city, :capacity, :start_date, :end_date)
   end
 
 end
